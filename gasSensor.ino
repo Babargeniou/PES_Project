d5da6e076d5d11d6bf59eb682173a59b7a870d37
Babar <47672311+Babargeniou@users.noreply.github.com>
2020-05-03 15:29:49 +0200

    Red, Green, Blue Leds  for pollution level

--- gasSensor.ino
+++ gasSensor.ino
@@ -17,15 +17,20 @@ Reading HTS221 Sensor
 AirQuality airqualitysensor;
 int current_quality =-1;
 const int pin19 = A0;   // input pin
-const int pin20 = A1;   // output pin
+
+const int ledPin = 22;    // Red led
+const int ledPin2 = 23;   // Green led
+const int ledPin3 = 24;   // Blue led
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
   airqualitysensor.init(A0);
-
-  pinMode(A1,OUTPUT);
+  
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT);
+  pinMode(24, OUTPUT);
 }
 
 void loop() {
@@ -36,33 +41,41 @@ void loop() {
     {
         if (current_quality ==0){
            // Serial.println("High pollution! Force signal active");
-            Serial.println("LED ON");
-            digitalWrite(pin20, HIGH);
+            Serial.println("Red LED ON");
+            digitalWrite(ledPin, LOW);            // Red
+            digitalWrite(ledPin2, HIGH);    
+            digitalWrite(ledPin3, HIGH);   
+           
         }
             
         else if (current_quality == 1){
          // Serial.println("High pollution!");
-            Serial.println("LED ON");
-            digitalWrite(pin20, HIGH);
+            Serial.println("Blue LED ON");
+            digitalWrite(ledPin, HIGH);          
+            digitalWrite(ledPin2, HIGH);           
+            digitalWrite(ledPin3, LOW);           // Blue
+        
         }
             
         else if (current_quality == 2){
            //Serial.println("Low pollution!");
-            Serial.println("LED OFF");
-            digitalWrite(pin20, LOW);
-        }
+            Serial.println(" Green LED ON");
+            digitalWrite(ledPin, HIGH);          
+            digitalWrite(ledPin2, LOW);             // Green
+            digitalWrite(ledPin3, HIGH);            
+         }
             
         else if (current_quality == 3){
           //Serial.println("Air fresh");
-            Serial.println("LED OFF");
-            digitalWrite(pin20, LOW);
+            Serial.println("LED OFF");      
+            digitalWrite(ledPin, HIGH);     
+            digitalWrite(ledPin2, HIGH);    
+            digitalWrite(ledPin3, HIGH);   
         }
      
     }
 
-//ISR(TIMER2_OVF_vect)
-//AttachInterrupt()
-//{
+
     //if(airqualitysensor.counter==122)//set 2 seconds as a detected duty
     if(airqualitysensor.counter==61)//set 2 seconds as a detected duty
     {
@@ -70,16 +83,15 @@ void loop() {
         airqualitysensor.first_val=analogRead(A0);
         airqualitysensor.counter=0;
         airqualitysensor.timer_index=1;
-       // PORTB=PORTB^0x20;
+   
     }
     else
     {
         airqualitysensor.counter++;
     }
-//}
 
      
-     
-     delay(100);
+     delay(500);
+     Serial.println("_____________________________________________________");
 
 }
