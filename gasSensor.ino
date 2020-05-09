c9158fa6f6b8c3737b7031f1ffc0a8343a9a7610
Mesticajerrin <64069482+Mesticajerrin@users.noreply.github.com>
2020-05-09 12:17:32 +0200

    Add files via upload

--- gasSensor.ino
+++ gasSensor.ino
@@ -13,20 +13,45 @@ Reading HTS221 Sensor
 
 #include"Air_Quality.h"
 #include"Arduino.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
 
 AirQuality airqualitysensor;
 int current_quality =-1;
 const int pin19 = A0;   // input pin
-const int pin20 = A1;   // output pin
+//const int pin20 = A1;   // output pin
+const int ledPin3 = 24;   // output pin
+String apiKey="AXF5A14LOCS6425F"; 
+ char* ssid="D-Link_DIR-600M";
+ char* password="Ahanaamruth04";
+const char* server="api.thingspeak.com";
+WiFiClient client;
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
+  Serial.println();
+  Serial.print("connecting to");
+  Serial.println(ssid);
+  WiFi.begin(ssid,password);
+  while(WiFi.status()!=WL_CONNECTED){
+    delay (500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("wifi connected");
+  Serial.println(WiFi.localIP());
   airqualitysensor.init(A0);
+  pinMode(24, OUTPUT);
+  }
+  
 
-  pinMode(A1,OUTPUT);
-}
+ // pinMode(D2,OUTPUT);
+ 
+
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -37,25 +62,25 @@ void loop() {
         if (current_quality ==0){
            // Serial.println("High pollution! Force signal active");
             Serial.println("LED ON");
-            digitalWrite(pin20, HIGH);
+            digitalWrite(ledPin3, LOW);
         }
             
         else if (current_quality == 1){
          // Serial.println("High pollution!");
             Serial.println("LED ON");
-            digitalWrite(pin20, HIGH);
+            digitalWrite(ledPin3, LOW);
         }
             
         else if (current_quality == 2){
            //Serial.println("Low pollution!");
             Serial.println("LED OFF");
-            digitalWrite(pin20, LOW);
+            digitalWrite(ledPin3, HIGH);
         }
             
         else if (current_quality == 3){
           //Serial.println("Air fresh");
             Serial.println("LED OFF");
-            digitalWrite(pin20, LOW);
+            digitalWrite(ledPin3, HIGH);
         }
      
     }
@@ -81,5 +106,23 @@ void loop() {
      
      
      delay(100);
+
+     if(client.connect(server,80)){
+      String postStr=apiKey;
+      postStr+="andfield1=";
+      postStr+=String(A0);
+      client.print("POST/update HTTP/1.1\n");
+      client.print("Host:api.thingspeak.com\n");
+      client.print("Connection:close\n");
+/*      client.print("X-THINGSPEAKAPI:"apiKey+"\n");*/
+      client.print("Content-Type:application/x/www/form/urlencoded\n");
+      client.print("Content-Length:");
+      client.print(postStr.length());
+      client.print("\n\n");
+      client.print(postStr);
+     }
+     client.stop();  
+     
+      
 
 }
